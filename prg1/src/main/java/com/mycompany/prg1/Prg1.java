e266b691ef30b05c13c3950344a775e03ab4feb7
aluno <aluno@lab02-04>
2026-03-02 14:05:31 -0300

    Minha primeira atualização ebaaaa

---
+++ prg1/src/main/java/com/mycompany/prg1/Prg1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prg1;
+
+/**
+ *
+ * @author aluno
+ */
+public class Prg1 {
+
+    public static void main(String[] args) {
+        System.out.println("Oieee!");
+    }
+}
